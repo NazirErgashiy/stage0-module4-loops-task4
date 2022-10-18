94073cdb95eec70e72ede930b4ad743891d0fc8c
NazirErgashiy <pikobiznes@gmail.com>
2022-10-18 23:07:12 +0500

    Two ranges sum skipped

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -3,5 +3,14 @@ package school.mjc.stage0.loops.task4;
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
 
+        int n = 1;
+        for (int i = 1; i <= lastPrinted; i++) {
+            if (n == 3) {
+                n = 1;
+                continue;
+            }
+            System.out.println(i);
+            n++;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -3,5 +3,18 @@ package school.mjc.stage0.loops.task4;
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
 
+        int n = 1;
+        int sum = 0;
+        for (int i = 1; i <= lastInRow; i++) {
+            if (n == numberToSkip) {
+                n = 0;
+                sum += i;
+            }
+            n++;
+            if (i == lastInRow) {
+                System.out.println(sum);
+            }
+        }
+
     }
 }
